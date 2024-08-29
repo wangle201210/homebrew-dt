fe43681da2619a3cbd6de22dd48aa631791327d5
wanna <wanna.w@binarywalk.com>
2024-08-29 17:58:45 +0800

    0.0.2

--- Formula/dt.rb
+++ Formula/dt.rb
@@ -2,11 +2,11 @@ class Dt < Formula
   desc "data-transfer，数据转换工具"
   homepage "https://github.com/wangle201210/dt"
   if Hardware::CPU.intel?
-      url "https://github.com/wangle201210/dt/releases/download/v0.0.1-alpha/dt_0.0.1.tar.gz"
-      sha256 "7bdf0fc1a80252bb7acc89dc557895f99f3955b1d54bd0ccbab20eab06ea8ded"
+      url "https://github.com/wangle201210/dt/releases/download/v0.0.2/dt_amd64_v0.0.2.tar.gz"
+      sha256 "f3f95d3de081ce652dd7d95bd8c70627e5a43a6fb5ecb533b4a3c9fe36806c5a"
     else
-       url "https://github.com/wangle201210/dt/releases/download/v0.0.1-alpha/dt_arm64_0.0.1.tar.gz"
-       sha256 "3fa102dbcca4e292d23462977089f31ee843d2a737729f377b0de94c743a82a7"
+       url "https://github.com/wangle201210/dt/releases/download/v0.0.2/dt_arm64_v0.0.2.tar.gz"
+       sha256 "ad08981f89b0a84cba8d93e6fff05884ad3d8619dd14e98d98d52c581661017d"
     end
   version "1.0.0"
 
